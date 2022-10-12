8a049234c669beb1c0bffa0c55ce8fb1212ba18a
Gabriel Ricardes <gabrielricardes16@gmail.com>
2022-10-12 16:04:48 -0300

    header js componet

--- vistas/Assets/js/menu.js
+++ vistas/Assets/js/menu.js
@@ -11,8 +11,8 @@ class  Menu extends HTMLElement{
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../css/estilo-home.css">
-        <link rel="stylesheet" href="../css/estilo-modal.css">
+        <link rel="stylesheet" href="Assets/css/estilo-home.css">
+        <link rel="stylesheet" href="Assets/css/estilo-modal.css">
     </head>
     <header class="header">
         <div class="logo-header">
