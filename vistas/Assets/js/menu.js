22803deb7e389133791b61bb4106c6be1d428896
Gabriel Ricardes <gabrielricardes16@gmail.com>
2022-10-12 21:19:36 -0300

    home proyecto

--- vistas/Assets/js/menu.js
+++ vistas/Assets/js/menu.js
@@ -13,10 +13,11 @@ class  Menu extends HTMLElement{
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="Assets/css/estilo-home.css">
         <link rel="stylesheet" href="Assets/css/estilo-modal.css">
+        <script src="https://kit.fontawesome.com/eb1dfef847.js" crossorigin="anonymous"></script>
     </head>
     <header class="header">
         <div class="logo-header">
-            <a href="home.html"><img src="Assets/img/logo.png" alt=""></a>
+            <a href="home.html"><img src="Assets/img/logo1.png" alt=""></a>
         </div>
         <div class="nav-menu">
             <input type="checkbox" id="check">    
@@ -27,7 +28,7 @@ class  Menu extends HTMLElement{
                 <li class="nav-menu--li"><a class="nav-menu--li-a" href="home.html">Home</a></li>
                 <li class="nav-menu--li"><a class="nav-menu--li-a" href="info.html">Info</a></li>
                 <li class="nav-menu--li"><a class="nav-menu--li-a"  href="contacto.html">Contacto</a></li>
-                <li class="nav-menu--li"><a class="nav-menu--li-a"  href="perfilUsuario.html">User</a></li>
+                <li class="nav-menu--li"><a class="nav-menu--li-a"  href="perfilUsuario.html"><i class="fa fa-user"></i></i></a></li>
                 <li class="nav-menu--li">
                     <input type="checkbox" id="btn-modal">
                     <label for="btn-modal" class="lbl-modal">unirme a proyecto</label>
